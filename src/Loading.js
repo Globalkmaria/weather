e21463a773a0b12c279a8c5d389e4e7ad0fdbe68
성현Maria 김 <globalkmaria@gmail.com>
2021-09-27 21:40:09 +0900

    [edit] change loading page

--- src/Loading.js
+++ src/Loading.js
@@ -1,66 +1,15 @@
 import React from 'react';
-import { IoMdSunny, IoMdMoon, IoMdSnow, IoIosCloudy } from 'react-icons/io';
-import {
-  IoThunderstorm,
-  IoCloudyNight,
-  IoPartlySunnySharp,
-  IoRainy,
-} from 'react-icons/io5';
 
 function Loading() {
   return (
     <div className="loading">
-      <div className="icon-box">
-        <div className="icon">
-          <IoMdSunny />
-        </div>
-        <div className="icon">
-          <IoMdMoon />
-        </div>
-        <div className="icon">
-          <IoMdSnow />
-        </div>
-        <div className="icon">
-          <IoIosCloudy />
-        </div>
-        <div className="icon">
-          <IoThunderstorm />
-        </div>
-        <div className="icon">
-          <IoCloudyNight />
-        </div>
-        <div className="icon">
-          <IoPartlySunnySharp />
-        </div>
-        <div className="icon">
-          <IoRainy />
-        </div>
-        <div className="icon">
-          <IoMdSunny />
-        </div>
-        <div className="icon">
-          <IoMdMoon />
-        </div>
-        <div className="icon">
-          <IoMdSnow />
-        </div>
-        <div className="icon">
-          <IoIosCloudy />
-        </div>
-        <div className="icon">
-          <IoThunderstorm />
-        </div>
-        <div className="icon">
-          <IoCloudyNight />
-        </div>
-        <div className="icon">
-          <IoPartlySunnySharp />
-        </div>
-        <div className="icon">
-          <IoRainy />
-        </div>
+      <div className="circle">
+        <span className="circle-btn">
+          <span className="circle__text">Loading ...</span>
+        </span>
+        <span className="circle__back-1"></span>
+        <span className="circle__back-2"></span>
       </div>
-      <span>Loading ...</span>
     </div>
   );
 }
